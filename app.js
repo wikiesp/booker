27eb6654b7399cba2cd256e52ec6587bcf3faedf
wikiesp <74647203+wikiesp@users.noreply.github.com>
2020-11-29 01:13:29 +0100

    Menu alineado con el logo
    Casi acabado, pendiente con el menu de responsive.

--- app.js
+++ app.js
@@ -7,13 +7,3 @@ addEventListener('DOMContentLoaded', () => {
 		});
 	}
 });
-
-// addEventListener('DOMContentLoaded', () => {
-// 	const btn_menu = document.querySelector('.btn_menu');
-// 	if (btn_menu) {
-// 		btn_menu.addEventListener('click', () => {
-// 			const menu_items = document.querySelector('.menu_items');
-// 			menu_items.classList.toggle('show');
-// 		});
-// 	}
-// });
