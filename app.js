4fcc55ad88055a80730c71c69d7b5bd86f2d2aff
wikiesp <74647203+wikiesp@users.noreply.github.com>
2020-12-08 20:34:08 +0100

    Web Responsive
    - Add animation
    - Add id, class html
    - Change css/js
    - Add elements

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// button responsive (hamburger)
+
 const burger = document.getElementById('hamburger');
 const liright = document.getElementById('nav_hamburger');
 
@@ -26,3 +28,37 @@ function topFunction() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
 }
+// Animations
+
+window.addEventListener('scroll', () => {
+	let currentPosition = document.documentElement.scrollTop;
+
+	const elements = {
+		scrollDown: {
+			// classes for scroll down, id is on the left and class is on the right
+			mainImage: 'left-to-right-main',
+			leftImage: 'left-to-right',
+			centerImage: 'down-to-up',
+			rightImage: 'rigth-to-down',
+			rightImage2: 'rigth-to-down',
+		},
+		scrollUp: {
+			// classes for scroll up
+			mainImage: 'right-to-left-main',
+			leftImage: 'right-to-left',
+			centerImage: 'up-to-down',
+			rightImage: 'left-to-right',
+			rightImage2: 'left-to-right',
+		},
+	};
+
+	Object.keys(elements.scrollDown).forEach(($element, $index, $array) => {
+		if (document.getElementById($element).offsetTop - 600 < currentPosition) {
+			document.getElementById($element).classList.add(elements.scrollDown[$element]);
+			document.getElementById($element).classList.remove(elements.scrollUp[$element]);
+		} else {
+			document.getElementById($element).classList.add(elements.scrollUp[$element]);
+			document.getElementById($element).classList.remove(elements.scrollDown[$element]);
+		}
+	});
+});
